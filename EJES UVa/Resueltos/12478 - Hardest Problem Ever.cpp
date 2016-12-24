f1c6ce12a0c1fcb0d93c1770f6e4895729813135
Alejandro Anzola <anzola95avila@gmail.com>
2016-12-23 20:39:42 -0500

    Resuelto UVa 12478

---
+++ EJES UVa/Resueltos/12478 - Hardest Problem Ever.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define loop(i, ii, ff) for(i = ii; i < ff; i++)
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+#define ABS(x) MAX((x), -(x))
+#define INRANGE(v, min, max) ((min) <= (v) && (v) <= (max))
+#define RANGE(v, min, max) MAX(MIN((v), (max)), (min))
+#define DEC(v, n) (v = ((v) + (n) - 1) % (n))
+#define INC(v, n) (v = ((v) +1) % (n))
+#define SZ 150000
+#define INF 1000000000
+
+using namespace std;
+
+int main() {
+    printf("KABIR\n");
+    return 0;
+}
