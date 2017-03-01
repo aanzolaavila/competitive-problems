472d6e2210cc7f47e1ced0eeb680cafa89fbe2f3
Alejandro Anzola <anzola95avila@gmail.com>
2017-03-01 05:43:33 -0500

    Pendiente UVa 11514

--- EJES UVa/11514 - Batman.cpp
+++ EJES UVa/11514 - Batman.cpp
@@ -7,88 +7,112 @@
 #define RANGE(v, min, max) MAX(MIN((v), (max)), (min))
 #define DEC(v, n) (v = ((v) + (n) - 1) % (n))
 #define INC(v, n) (v = ((v) +1) % (n))
-#define SZ 110
-#define N 1010
-#define INF 1000000000
+#define SZ 200
+#define N 10010
+#define INF 10000000000
 
 using namespace std;
 
+typedef long long ll;
+
 int p, v, e;
 
-struct power {
-    string name;
-    int attack;
-    int energy;
-    power(string n, int a, int c): name(n), attack(a), energy(c) {}
-};
+ll mem[N][N];
 
-struct villain {
-    int defense;
-    set<string> vuln;
-};
+//string p_name[N];
+int p_e[N];
+int p_a[N];
+map<string, int> ids;
+bool can[N][N];
+//set<string> v_power[N];
+int v_def[N];
 
-vector<power> powers;
-//map<string, power> powers_n;
-vector<villain> villains;
+/*ll batman(int i, int j) {
+    ll &mi = mem[i][j];
+    if (mi != -1) return mi;
+    if (j == v) return mi = 0;
+    if (i == p && j < v) return mi = INF;
 
-bool solve();
-bool calc(int, int, int);
+    return mi = (v_power[j].count(p_name[i]) != 0 && p_a[i] >= v_def[j]) ?
+	min(p_e[i] + batman(i+1, j+1), batman(i+1, j)) : batman(i+1, j);
+	}*/
+
+ll batman() {
+    int i, j;
+
+    loop(i, 0, p+1) {
+	mem[i][v] = 0;
+    }
+
+    loop(i, 0, v) {
+	mem[p][i] = INF;
+    }
+
+    for(i = p-1; i >= 0; i--) {
+	for (j = v-1; j >= 0; j--) {
+	    if (can[i][j] && p_a[i] >= v_def[j]) {
+		mem[i][j] = min(p_e[i] + mem[i+1][j+1], mem[i+1][j]);
+	    } else {
+		mem[i][j] = mem[i+1][j];
+	    }
+	}
+    }
+
+    return mem[0][0];
+    
+}
 
 int main() {
-    int i;
-    char tmp[SZ];
-    char tmp2[SZ];
+    char in[SZ];
+    char name[SZ];
     char * t;
-    int a, c;
-    
-    while(scanf("%d%d%d", &p, &v, &e), p|v|e) {
-	powers.clear();
-	//powers_n.clear();
-	villains.clear();
-
+    int i, j;
+    while(scanf("%d %d %d\n", &p, &v, &e), p|v|e) {
+	ids.clear();
+	
+	loop(i, 0, p+2) {
+	    loop(j, 0, v+2) {
+		can[i][j] = false;
+	    }
+	}
+	
 	loop(i, 0, p) {
-	    scanf("%s %d %d", tmp, &a, &c);
-	    string n(tmp);
-	    power po(n, a, c);
-	    powers.push_back(po);
-	    //powers_n[n] = po;
+	    scanf("%s %d %d\n", in, p_a+i, p_e+i);
+	    //printf("%s\n", in);
+	    //p_name[i] = in;
+	    ids[in] = i;
 	}
 
 	loop(i, 0, v) {
-	    villain v;
-	    scanf("%*s %d %s\n", &a, tmp2);
-	    printf("%d %s : ", a, tmp2);
-	    v.defense = a;
-	    t = strtok(tmp2, ",");
-	    while(t != NULL) {
-		printf("%s ", t);
-		v.vuln.insert(string(t));
-		t = strtok(NULL, ",");
+	    scanf("%*s %d %s\n", v_def+i, in);
+	    //v_power[i].clear();
+	    int n = strlen(in);
+	    loop(j, 0, n) {
+		if (in[j] == ',') in[j] = ' ';
 	    }
-	    puts("");
-	    //sort(v.vuln.begin(), v.vuln.end());
-	    villains.push_back(v);
+
+	    istringstream is(in);
+	    while(is >> name) {
+		//v_power[i].insert(name);
+		can[ids[name]][i] = true;
+	    }
 	}
 
-	printf("%s\n", solve() ? "Yes" : "No");
+	/*loop(i, 0, p+2) {
+	    loop(j, 0, v+2) {
+		mem[i][j] = -1;
+	    }
+	    }*/
+
+	printf("%s\n", batman() <= e ? "Yes": "No");
+
+	/*loop(i, 0, p+1) {
+	    loop(j, 0, v+1) {
+		printf("%d ", mem[i][j]);
+	    }
+	    puts("");
+	    }*/
     }
 
     return 0;
-}
-
-int mem[N][N][2];
-
-bool solve() {
-    int i, j;
-    loop(i, 0, N) {
-	loop(j, 0, N) {
-	    mem[i][j][0] = -1;
-	    mem[i][j][1] = INF;
-	}
-    }
-    return calc(0, 0, e);
-}
-
-bool calc(int i, int j, int e) {
-    
 }
