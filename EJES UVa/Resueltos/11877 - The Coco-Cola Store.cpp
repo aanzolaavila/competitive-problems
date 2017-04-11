8aad8c0d153d20a05d2d1bd7408a3f80da3984d2
Alejandro Anzola <anzola95avila@gmail.com>
2017-04-10 19:18:20 -0500

    Resuelto UVa 11877

---
+++ EJES UVa/Resueltos/11877 - The Coco-Cola Store.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define loop(i, ii, ff) for(i = ii; i < ff; i++)
+#define loopd(i, ii, ff) for(i = ii; i >= ff; i--)
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+#define ABS(x) MAX((x), -(x))
+#define INRANGE(v, min, max) ((min) <= (v) && (v) <= (max))
+#define RANGE(v, min, max) MAX(MIN((v), (max)), (min))
+#define DEC(v, n) (v = ((v) + (n) - 1) % (n))
+#define INC(v, n) (v = ((v) +1) % (n))
+#define SZ 1000
+#define INF 1000000000
+#define EPS 1e-9
+
+using namespace std;
+
+int main() {
+    int n;
+    while(scanf("%d", &n), n) {
+	printf("%d\n", n>>1);
+    }
+
+    return 0;
+}
