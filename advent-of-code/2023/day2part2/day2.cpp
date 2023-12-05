9400d9c9a3ed65560b9f866e7823bc1caa79ad2a
Alejandro Anzola <me@alejandroanzola.info>
2023-12-04 22:02:46 -0500

    solves aoc 2023 day 2

---
+++ advent-of-code/2023/day2part2/day2.cpp
@@ -0,0 +1,97 @@
+#ifdef __APPLE__
+#include "../../stdc++.h"
+#else
+#include <bits/stdc++.h>
+#endif
+
+#define F first
+#define S second
+#define MP make_pair
+#define PB push_back
+#define INF 100000000
+// or use numeric_limits<type>::max()
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+#define ABS(x) MAX((x), -(x))
+#define INRANGE(v, min, max) ((min) <= (v) && (v) <= (max))
+#define RANGE(v, min, max) MAX(MIN((v), (max)), (min))
+#define DEC(v, n) (v = ((v) + (n)-1) % (n))
+#define INC(v, n) (v = ((v) + 1) % (n))
+#define LOOP(i, ii, ff) for (i = (ii); i < (ff); i++)
+#define LSOne(i) ((i) & -(i))
+
+#define NDEBUG
+#ifndef NDEBUG
+#define dprintf(...)                                                           \
+  do {                                                                         \
+    fprintf(stderr, "DEBUG: " __VA_ARGS__);                                    \
+  } while (0);
+#else
+#define dprintf(...) /**/
+#endif
+
+#define SZ 10000
+
+using namespace std;
+
+char buff[SZ];
+
+int calc(char *ptr) {
+  int c;
+  int read;
+  char bag[10];
+
+  struct {
+    int red;
+    int green;
+    int blue;
+  } cubes;
+  memset(&cubes, 0, sizeof(cubes));
+
+  while (*ptr != '\0') {
+    sscanf(ptr, "%d %[^,;]%n", &c, bag, &read);
+    ptr += read;
+    dprintf("%d %s\n", c, bag);
+
+    if (strcmp("red", bag) == 0) {
+      cubes.red = MAX(cubes.red, c);
+    } else if (strcmp("green", bag) == 0) {
+      cubes.green = MAX(cubes.green, c);
+    } else {
+      assert(strcmp("blue", bag) == 0);
+      cubes.blue = MAX(cubes.blue, c);
+    }
+
+    if (*ptr++ == ';') {
+      // memset(&cubes, 0, sizeof(cubes));
+      dprintf("reset\n");
+    }
+  }
+
+  return cubes.red * cubes.green * cubes.blue;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int s = 0;
+  while (fgets(buff, SZ, stdin) != NULL) {
+    int l = strlen(buff);
+    if (buff[l - 1] == '\n') {
+      buff[--l] = '\0';
+    }
+
+    int game;
+    int n;
+    sscanf(buff, "Game %d: %n\n", &game, &n);
+
+    dprintf("%s\n", buff + n);
+
+    s += calc(buff + n);
+  }
+
+  printf("%d\n", s);
+
+  return 0;
+}
